4e36ad7b803d3b4345a7e76e2a0268481831764a
rodrigorparedes <rodrigorparedes@gmail.com>
2025-07-07 12:01:36 -0400

    addd icon

--- src/App.jsx
+++ src/App.jsx
@@ -12,10 +12,11 @@ function App() {
 
   return (
     <>
+{/*        <a href="https://react.dev" target="_blank" >
+          <img src={icon} className="logo" alt="Wather logo" />
+        </a> */}
 {/*       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
+     
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
